1a997ab483f3bf3438e0d3eec2bd55e511433c66
Juana Asam <Asam.Juana@schueler.6072-fuerth.de>
2025-12-16 07:54:04 +0100

    #1_main_routes implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { RouterModule, Routes } from '@angular/router';
 import { RoutesTo } from './utilities/routes';
 import { NgModule } from '@angular/core';
-import { App } from './app';
 import { ControllerComponent } from './controller-component/controller-component';
 import { GameComponent } from './game-component/game-component';
 
